812a8e00948054a358743864a0f934c4c713476b
Dean Attali <daattali@gmail.com>
2022-06-05 00:23:49 +0300

    finish v1 functionality: make shinytip work in Rmd and in shinyalert; add 'pointer' param, make it work with img/input/icons, add tip_icon() and tip_label() functions

--- R/tip.R
+++ R/tip.R
@@ -8,6 +8,7 @@ tip <- function(
     fg = getOption("shinytip.fg", "white"),
     size = getOption("shinytip.size", "12px"),
     animate = getOption("shinytip.animate", TRUE),
+    pointer = getOption("shinytip.pointer", TRUE),
     ...) {
 
   allowed_position <- c("up", "down", "left", "right", "up-left", "up-right", "down-left", "down-right")
@@ -29,24 +30,32 @@ tip <- function(
     length <- "xlarge"
   }
 
-  shiny::addResourcePath("shinytip-assets",
-                         system.file("www", package = "shinytip"))
+  if (missing(content) || !nzchar(trimws(content))) {
+    stop("tip: Must proide non-empty content", call. = FALSE)
+  }
 
   if (is.numeric(size)) {
     size <- paste0(size, "px")
   }
 
   css <- paste0(
-    "--balloon-color: ", bg, ";\n",
-    "--balloon-text-color: ", fg, ";\n",
-    "--balloon-font-size: ", size, ";"
+    "--balloon-color: ", bg, "; ",
+    "--balloon-text-color: ", fg, "; ",
+    "--balloon-font-size: ", size, "; "
   )
+
+  if (!pointer) {
+    css <- paste0(css, "cursor: inherit; ")
+  }
 
   if (is.null(tag) || is.na(tag) || length(tag) == 0) {
     stop("tip: `tag` must not be empty", call. = FALSE)
   }
 
-  if (!inherits(tag, "shiny.tag")) {
+  if (inherits(tag, "shiny.tag") && (tag$name %in% c("img", "input", "i"))) {
+    tag <- shiny::div(tag)
+    css <- paste0(css, "display: inline-block; ")
+  } else if (!inherits(tag, "shiny.tag")) {
     tag <- shiny::span(tag)
   }
 
@@ -72,15 +81,34 @@ tip <- function(
     )
   }
 
-  shiny::tagList(
-    shiny::singleton(
-      shiny::tags$head(
-        shiny::tags$link(
-          rel = "stylesheet",
-          href = file.path("shinytip-assets", "lib", "balloon-1.2.0", "balloon.min.css")
-        )
-      )
-    ),
-    tag
+  runtime <- knitr::opts_knit$get("rmarkdown.runtime")
+  if (!is.null(runtime) && runtime == "shiny") {
+    # we're inside an Rmd document
+    insert_into_doc <- shiny::tagList
+  } else {
+    # we're in a shiny app
+    insert_into_doc <- shiny::tags$head
+  }
+
+  shiny::addResourcePath("shinytip-assets", system.file("www", package = "shinytip"))
+
+  htmltools::attachDependencies(
+    tag,
+    htmltools::htmlDependency(
+      name = "balloon",
+      version = "1.2.0",
+      src = c(href = "shinytip-assets/lib"),
+      stylesheet = "balloon-1.2.0/balloon.min.css"
+    )
   )
+}
+
+#' @export
+tip_icon <- function(...) {
+  tip(shiny::icon("question-circle"), ...)
+}
+
+#' @export
+tip_label <- function(label, ...) {
+  shiny::tagList(label, tip_icon(...))
 }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # shinytip
 Simple flexible tootips for Shiny apps
+
+works in rmd
+works in dynamic ui (insertUI or renderUI)
+works in modals 
+works in modules
+
+does not automatically detect the best position
+only on hoer, not on click
+
+Balloon.css make use of pseudo-elements, so if pseudo elements are already in use on an element, the tooltip will conflict with them resulting in potential bugs.
+
+
+set option for all tooltips using `shinytip.optionname`
+
+
+tippy doesn't work in all the scenarios i tried and doesnt support the optoins i wanted, although it does have a different et of options/methods that shinytip doesnt have so take a look there as well i shinytip doesnt do what you want
